d642201dae760a8a74c06ef2e5de76502b03f71a
ahmed omer <ahmedjomer@gmail.com>
2021-05-08 14:52:30 -0700

    user schema data points

---
+++ routes/Users.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const User = require('../models/User');
+
+// Registration
+try {
+    
+} catch (error) {
+    
+}
+
+// Login
+try {
+    
+} catch (error) {
+    
+}
